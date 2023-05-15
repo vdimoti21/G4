cf5c5a8a467d59586c287ef08fbdb48f05e779fc
Vedran Đimoti <vdimoti21@foi.hr>
2023-05-15 17:12:47 +0200

    pokuaj 2 repository

---
+++ Evaluation Manager (4)/Repositrorys/Student_repository.cs
@@ -0,0 +1,14 @@
+﻿using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories {
+    public class Student_repository {
+        public static List<Student> GetStudents() {
+
+        }
+    }
+}
---
+++ Evaluation Manager (4)/Repositrorys/teacher_reository.cs
@@ -0,0 +1,49 @@
+﻿using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories {
+    public static class TeacherReository
+    {
+        public static Teacher GetTeacher(string username) {
+            string sql = $"SELECT * FROM Teachers WHERE username = '{username}'";
+            return FetchTeacher(sql);
+        }
+        public static Teacher GetTeacher(int id) {
+            string sql = $"SELECT * FROM Teachers WHERE Id = '{id}'";
+            return FetchTeacher(sql);
+        }
+        private static Teacher FetchTeacher(string sql) {
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            Teacher teacher = null;
+            if (reader.HasRows) {
+                reader.Read();
+                teacher = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return teacher;
+        }
+        private static Teacher CreateObject(SqlDataReader reader) {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            string username = reader["Username"].ToString();
+            string password = reader["Password"].ToString();
+
+            var teacher = new Teacher {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Password = password
+            };
+            return teacher;
+        }
+    }
+}
